3848e12778524c10e28282f70b99f60336994172
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-09 10:05:27 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-0600.bash
+++ Exercises/02. Commands/Solutions/02-a-0600.bash
@@ -2,6 +2,24 @@
 
 
 
+# Solution 1 - Multiple Arguments
+
+mkdir -p ~/practice/01
+
+cd ~/practice/01 || exit
+
+touch f1 f2 f3
+
+mkdir -p ~/dir1
+
+cp f1 ~/dir1
+cp f2 ~/dir2
+cp f3 ~/dir3
+
+
+
+# Solution 2 - Single Arguments
+
 mkdir -p ~/practice/01
 
 touch ~/practice/01/f{1,2,3}
