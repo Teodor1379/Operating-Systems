cb0d7c9fee8f17a936c8add300aa56521f8dd440
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-18 16:47:53 +0300

    Commit: Exercises - 03. Pipes - Solutions

--- Exercises/03. Pipes/Solutions/03-b-3400.bash
+++ Exercises/03. Pipes/Solutions/03-b-3400.bash
@@ -2,4 +2,12 @@
 
 
 
-cat /etc/services | grep "#" | wc -l
+# Solution 1 - Part-Line Comments
+
+cat /etc/services | grep "#" | wc -l;
+
+
+
+# Solution 2 - Full-Line Comments
+
+cat /etc/services | grep "^#" | wc -l;
