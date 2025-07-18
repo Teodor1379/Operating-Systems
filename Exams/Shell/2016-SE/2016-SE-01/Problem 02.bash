0f0cff8251b14584930c2404bf195a86d777fa86
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-18 14:30:19 +0300

    Commit: Exams - Shell - 2016-SE-01

---
+++ Exams/Shell/2016-SE/2016-SE-01/Problem 02.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+# 2016-SE-01
+
+
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Невалиден Брой Аргументи!";
+
+    exit 1;
+fi
+
+if [[ ! -d "${1}" ]]; then
+    echo "Грешен Аргумент - НЕ Е ДИРЕКТОРИЯ!";
+
+    exit 2;
+fi
+
+if [[ ! -r "${1}" ]]; then
+    echo "Грешен Аргумент - НЕ Е ЧЕТИМ!";
+
+    exit 3;
+fi
+
+
+
+find "${1}" -mindepth 1 -maxdepth 2 -type l ! -exec test -e {} \; -print 2> /dev/null
