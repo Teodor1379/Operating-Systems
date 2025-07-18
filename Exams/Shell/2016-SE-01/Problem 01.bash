75cf6eda29b7af87eb2d20b2f2ad49ad21ec3bbb
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-07-18 11:31:12 +0300

    Commit: Exams - Shell - 2016-SE-01

--- Exams/Shell/2016-SE-01/Problem 01.bash
+++ Exams/Shell/2016-SE-01/Problem 01.bash
@@ -16,19 +16,19 @@ if [[ $# -ne 1 ]]; then
     exit 1;
 fi
 
-if [[ ! -f "$1" ]]; then
+if [[ ! -f "${1}" ]]; then
     echo "Грешка на Аргумента - НЕ Е ФАЙЛ!";
 
     exit 2;
 fi
 
-if [[ ! -r "$1" ]]; then
+if [[ ! -r "${1}" ]]; then
     echo "Грешка на Аргумента - НЕЧЕТИМ!";
 
     exit 3;
 fi
 
-if [[ "$1" !=  "$SPECIAL_NAME" ]]; then
+if [[ "${1}" !=  "${SPECIAL_NAME}" ]]; then
     echo "Грешка на Аргумента - НЕВАЛИДНО ИМЕ!";
 
     exit 4;
@@ -36,6 +36,6 @@ fi
 
 
 
-RESULT=$(cat ${SPECIAL_NAME} | grep -E "[0,2,4,6,8]+" | grep -v -E "[a-w]+" | wc -l);
+RESULT=$(cat "${SPECIAL_NAME}" | grep -E "[02468]+" | grep -v -E "[a-w]+" | wc -l);
 
 echo "Броят на търсените редове е ${RESULT}.";
