fcf84f1655e1af6d2e106582e730f5b7c293948d
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 18:23:38 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-4000.bash
+++ Exercises/02. Commands/Solutions/02-a-4000.bash
@@ -6,8 +6,8 @@
 
 
 
-touch ~/permissions.txt;
+touch ~/permissions.txt
 
-chmod 435 ~/permissions.txt;
+chmod 435 ~/permissions.txt
 
-chmod u=r,g=wx,o=rx ~/permissions.txt;
+chmod u=r,g=wx,o=rx ~/permissions.txt
