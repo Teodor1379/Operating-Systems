b3c5d52bd603d9c7a3e9eff9e1486799a8befff4
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 06:50:45 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-0600.bash
+++ Exercises/02. Commands/Solutions/02-a-0600.bash
@@ -8,26 +8,26 @@
 
 # Solution 1 - Multiple Arguments
 
-mkdir -p ~/practice/01
+mkdir -p ~/practice/01;
 
-cd ~/practice/01 || exit
+cd ~/practice/01 || exit;
 
-touch f1 f2 f3
+touch f1 f2 f3;
 
-mkdir -p ~/dir1
+mkdir -p ~/dir1;
 
-cp f1 ~/dir1
-cp f2 ~/dir2
-cp f3 ~/dir3
+cp f1 ~/dir1;
+cp f2 ~/dir2;
+cp f3 ~/dir3;
 
 
 
 # Solution 2 - Single Argument
 
-mkdir -p ~/practice/01
+mkdir -p ~/practice/01;
 
-touch ~/practice/01/f{1,2,3}
+touch ~/practice/01/f{1,2,3};
 
-mkdir -p ~/dir1
+mkdir -p ~/dir1;
 
-cp ~/practice/01/f{1,2,3} ~/dir1/
+cp ~/practice/01/f{1,2,3} ~/dir1/;
