f18922422cd3f26e09102b8ca5f72aeeb9b790da
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 18:34:47 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-b-4000.bash
+++ Exercises/02. Commands/Solutions/02-b-4000.bash
@@ -6,4 +6,4 @@
 
 
 
-cp "$(find /etc/ -type f -exec wc -c {} ';' 2> /dev/null | sort -n -k 1 | head -n 1 | cut -d ' ' -f 2-)" ~;
+cp "$(find /etc/ -type f -exec wc -c {} ';' 2> /dev/null | sort -n -k 1 | head -n 1 | cut -d ' ' -f 2-)" ~
