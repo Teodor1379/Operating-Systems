ff6eded79d27101e4adc3d2f08c2b7b630d634c7
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 18:16:56 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-0100.bash
+++ Exercises/02. Commands/Solutions/02-a-0100.bash
@@ -8,10 +8,10 @@
 
 # Solution 1 - Absolute Path
 
-cp /etc/passwd ~/my_passwd;
+cp /etc/passwd ~/my_passwd
 
 
 
 # Solution 2 - Relative Path ~
 
-cp /etc/passwd my_passwd;
+cp /etc/passwd my_passwd
