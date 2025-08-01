934c09a97afd6b7d440f1d513b23dc9ffb87aa04
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 06:51:48 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-0601.bash
+++ Exercises/02. Commands/Solutions/02-a-0601.bash
@@ -6,14 +6,14 @@
 
 
 
-mkdir -p ~/practice/01
+mkdir -p ~/practice/01;
 
-touch ~/practice/01/f{1,2,3}
+touch ~/practice/01/f{1,2,3};
 
-mkdir -p ~/dir1
+mkdir -p ~/dir1;
 
-cp ~/practice/01/f{1,2,3} ~/dir1/
+cp ~/practice/01/f{1,2,3} ~/dir1/;
 
-mkdir -p ~/dir2
+mkdir -p ~/dir2;
 
-mv ~/dir1/f2 ~/dir2/numbers
+mv ~/dir1/f2 ~/dir2/numbers;
