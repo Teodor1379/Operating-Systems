f48d8331eb246b5a2f153c63b6abba98c9f071d0
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 07:08:56 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-8000.bash
+++ Exercises/02. Commands/Solutions/02-a-8000.bash
@@ -2,6 +2,20 @@
 
 
 
-mkdir -p ~/myetc
+# 02-a-8000
 
-find /etc -perm 444 -exec cp {} ~/myetc \;
+
+
+# Solution 1 - Chars
+
+mkdir -p ~/myetc;
+
+find /etc -perm -u+r,g+r,o+r -exec cp '{}' ~/myetc ';' 2> /dev/null;
+
+
+
+# Solution 2 - Digits
+
+mkdir -p ~/myetc;
+
+find /etc -perm -444 -exec cp '{}' ~/myetc ';' 2> /dev/null;
