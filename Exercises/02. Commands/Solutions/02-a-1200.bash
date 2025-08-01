d716e3dddf6572f186f4f0bcd1d7121da3e6aa63
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 06:52:34 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-1200.bash
+++ Exercises/02. Commands/Solutions/02-a-1200.bash
@@ -2,12 +2,16 @@
 
 
 
+# 02-a-1200
+
+
+
 # Solution 1 - Full Names
 
-find /home -type d 2> /dev/null
+find /home -type d 2> /dev/null;
 
 
 
 # Solution 2 - Single Names
 
-find /home -type d -exec basename {} \; 2> /dev/null
+find /home -type d -exec basename {} \; 2> /dev/null;
