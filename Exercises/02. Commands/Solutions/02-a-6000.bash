4f71e0e04333bf147f86f57f3a845ab0275edfdb
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 18:32:17 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-6000.bash
+++ Exercises/02. Commands/Solutions/02-a-6000.bash
@@ -8,10 +8,10 @@
 
 # Solution 1 - Chars
 
-find /bin -type f -perm -a=rwx 2> /dev/null;
+find /bin -type f -perm -a=rwx 2> /dev/null
 
 
 
 # Solution 2 - Digits
 
-find /bin -type f -perm 777 2> /dev/null;
+find /bin -type f -perm 777 2> /dev/null
