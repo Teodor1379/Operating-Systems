0b29d3bad8bf87c84782e8db75bbcb8be7c15893
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-01 07:06:38 +0300

    Commit: Exercises - 02. Commands - Solutions

--- Exercises/02. Commands/Solutions/02-a-5506.bash
+++ Exercises/02. Commands/Solutions/02-a-5506.bash
@@ -2,11 +2,16 @@
 
 
 
+# 02-a-5506
+
+
+
 # Solution 1 - Safe
 
-find ~ -type f -newer ~/practice/01/f3 -exec rm -i {} \;
+find ~ -type f -newer ~/practice/01/f3 -exec rm -i '{}' ';';
+
 
 
 # Solution 2 - Unsafe
 
-find ~ -type f -newer ~/practice/01/f3 -delete
+find ~ -type f -newer ~/practice/01/f3 -delete;
