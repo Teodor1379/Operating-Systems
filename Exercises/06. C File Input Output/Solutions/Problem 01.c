2b32f0303fdb9cec047661ee6756bb638bbaa0f0
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2025-08-27 12:14:41 +0300

    Commit: 06. C File Input Output - Solutions

---
+++ Exercises/06. C File Input Output/Solutions/Problem 01.c
@@ -0,0 +1,145 @@
+#include <assert.h>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <fcntl.h>
+
+#include <err.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+
+const char* ERROR_FILE_O = "Error while opening the file!";
+const char* ERROR_FILE_C = "Error while closing the file!";
+const char* ERROR_FILE_R = "Error while reading the file!";
+const char* ERROR_FILE_W = "Error while writing the file!";
+
+
+
+void openingSafe(const char*, int, mode_t, int* );
+void closingSafe(int                            );
+
+
+
+void closingSafeAllDesc(void);
+
+
+
+int    readingSafe(const char*, int, void*, size_t);
+int    writingSafe(const char*, int, void*, size_t);
+
+
+
+const uint8_t count = 2;
+
+int descriptors[2] = { -1, -1 };
+
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        errx(1, "Expected Different Arguments!");
+    }
+
+
+    const char* file1 = argv[1];
+    const char* file2 = argv[2];
+
+    openingSafe(file1,  O_RDONLY                  , S_IRUSR           , &descriptors[0]);
+    openingSafe(file2,  O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR , &descriptors[1]);
+
+
+    while (1) {
+        char c = 0;
+
+        if (readingSafe(file1, descriptors[0], &c, sizeof(char)) == 0) {
+            break;
+        }
+
+        writingSafe(file2, descriptors[1], &c, sizeof(c));
+    }
+
+
+    closingSafeAllDesc();
+
+
+    exit(EXIT_SUCCESS);
+}
+
+
+
+void openingSafe(const char* filePath, int flags, mode_t mode, int* memory) {
+    assert(filePath !=  NULL);
+    assert(memory   !=  NULL);
+
+    *memory = open(filePath, flags, mode);
+
+    if (*memory == -1) {
+        closingSafeAllDesc();
+
+        errx(2, "%s: %s", ERROR_FILE_O, filePath);
+    }
+}
+
+
+
+void closingSafe(int descriptor) {
+    int oldErrno = errno;
+
+    if (descriptor >= 0) {
+        int closing = close(descriptor);
+
+        if (closing == -1) {
+            warnx("%s: %d", ERROR_FILE_C, descriptor);
+        }
+    }
+
+    errno = oldErrno;
+}
+
+
+
+void closingSafeAllDesc() {
+    for (uint8_t i = 0; i < count; ++i) {
+        closingSafe(descriptors[i]);
+    }
+}
+
+
+
+int readingSafe(const char* filePath, int descriptor, void* memory, size_t size) {
+    assert(filePath !=  NULL);
+    assert(memory   !=  NULL);
+
+    int result = read(descriptor, memory, size);
+
+    if (result == -1) {
+        closingSafeAllDesc();
+
+        errx(3, "%s: %s", ERROR_FILE_R, filePath);
+    }
+
+    return result;
+}
+
+int writingSafe(const char* filePath, int descriptor, void* memory, size_t size) {
+    assert(filePath !=  NULL);
+    assert(memory   !=  NULL);
+
+    int result = write(descriptor, memory, size);
+
+    if (result == -1 || (size_t)(result) != size) {
+        closingSafeAllDesc();
+
+        errx(4, "%s: %s", ERROR_FILE_W, filePath);
+    }
+
+    return result;
+}
